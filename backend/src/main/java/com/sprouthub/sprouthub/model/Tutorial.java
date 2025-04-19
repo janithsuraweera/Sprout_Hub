673604cd7695ddfef070cc6f7e0817e73cfadcfd
Shehara0 <sheharasiriwardana21@gmail.com>
2025-04-19 21:05:26 +0530

    add tutorial backend part

--- backend/src/main/java/com/sprouthub/sprouthub/model/Tutorial.java
+++ backend/src/main/java/com/sprouthub/sprouthub/model/Tutorial.java
@@ -1,1 +1,20 @@
-//Tutorial part
+package com.sprouthub.sprouthub.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "tutorials")
+@Data
+public class Tutorial {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String videoUrl;
+    private Date createdAt;
+    private String userId; // This is the user who created the tutorial
+}
