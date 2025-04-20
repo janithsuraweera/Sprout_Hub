2560def019ca1e443b84e92e54c8202995c56153
Dinuwarabandara5 <dinuwarabandara5@gmail.com>
2025-04-20 18:38:58 +0530

    post edit

--- frontend/src/components/Posts/EditPost.jsx
+++ frontend/src/components/Posts/EditPost.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import postService from '../../services/postService';
-
+//post edit 
 function EditPost() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
