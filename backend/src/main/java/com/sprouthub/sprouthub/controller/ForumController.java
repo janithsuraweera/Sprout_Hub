e2a1e93a901c1e600067505c717a37173c580ee5
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-23 17:03:52 +0530

    add function(a, b) { return a + b; }

--- backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
+++ backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
@@ -7,37 +7,3 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@RestController
-@RequestMapping("/api/forum")
-public class ForumController {
-
-    @Autowired
-    private ForumService forumService;
-
-    @PostMapping
-    public ResponseEntity<ForumPost> createForumPost(@RequestBody ForumPost forumPost) {
-        return ResponseEntity.ok(forumService.createForumPost(forumPost));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ForumPost> getForumPostById(@PathVariable String id) {
-        return ResponseEntity.ok(forumService.getForumPostById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<ForumPost>> getAllForumPosts() {
-        return ResponseEntity.ok(forumService.getAllForumPosts());
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ForumPost> updateForumPost(@PathVariable String id, @RequestBody ForumPost forumPost) {
-        return ResponseEntity.ok(forumService.updateForumPost(id, forumPost));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteForumPost(@PathVariable String id) {
-        forumService.deleteForumPost(id);
-        return ResponseEntity.ok().build();
-    }
-}
