45266e412591591b183b0a890ea1069d9e3fdc04
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-23 17:19:53 +0530

    buttons chang

--- frontend/src/components/Forum/EditForumPost.jsx
+++ frontend/src/components/Forum/EditForumPost.jsx
@@ -52,7 +52,7 @@ function EditForumPost() {
           </div>
           <button
             type="submit"
-            className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+            className="w-full py-2 px-4 bg-blue-400 text-white rounded hover:bg-blue-700"
           >
             Update Post
           </button>
