f921c4585ae9bb8629e52f9ec6dd3d01b0103a3d
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-23 17:16:35 +0530

    add exception class

--- backend/src/main/java/com/sprouthub/sprouthub/exception/ForumNotFoundException.java
+++ backend/src/main/java/com/sprouthub/sprouthub/exception/ForumNotFoundException.java
@@ -1,7 +1,5 @@
 package com.sprouthub.sprouthub.exception;
 
 public class ForumNotFoundException extends RuntimeException {
-    public ForumNotFoundException(String message) {
-        super(message);
-    }
+
 }
