f7a1e09aac7360d84165bddeac42a0859a5afe68
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-23 17:05:43 +0530

    all add

--- backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
+++ backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
@@ -1,3 +1,5 @@
+
+
 package com.sprouthub.sprouthub.controller;
 
 import com.sprouthub.sprouthub.model.ForumPost;
@@ -19,3 +21,25 @@ public class ForumController {
     public ResponseEntity<ForumPost> createForumPost(@RequestBody ForumPost forumPost) {
         return ResponseEntity.ok(forumService.createForumPost(forumPost));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ForumPost> getForumPostById(@PathVariable String id) {
+        return ResponseEntity.ok(forumService.getForumPostById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ForumPost>> getAllForumPosts() {
+        return ResponseEntity.ok(forumService.getAllForumPosts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ForumPost> updateForumPost(@PathVariable String id, @RequestBody ForumPost forumPost) {
+        return ResponseEntity.ok(forumService.updateForumPost(id, forumPost));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteForumPost(@PathVariable String id) {
+        forumService.deleteForumPost(id);
+        return ResponseEntity.ok().build();
+    }
+}
