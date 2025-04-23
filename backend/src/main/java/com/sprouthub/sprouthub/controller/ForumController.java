b191257f05bed33f75760bd335fdb8558dfcb01f
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-23 17:04:53 +0530

    add all functions

--- backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
+++ backend/src/main/java/com/sprouthub/sprouthub/controller/ForumController.java
@@ -7,3 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RestController
+@RequestMapping("/api/forum")
+public class ForumController {
+
+    @Autowired
+    private ForumService forumService;
+
+    @PostMapping
+    public ResponseEntity<ForumPost> createForumPost(@RequestBody ForumPost forumPost) {
+        return ResponseEntity.ok(forumService.createForumPost(forumPost));
+    }
