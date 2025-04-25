bd2006f352d377e97b4e04e58902faed5d7c5c43
Shehara0 <sheharasiriwardana21@gmail.com>
2025-04-26 00:45:05 +0530

    added url mapping

---
+++ backend/src/main/java/com/sprouthub/sprouthub/controller/TutorialController.java
@@ -0,0 +1,32 @@
+package com.sprouthub.sprouthub.controller;
+
+import com.sprouthub.sprouthub.model.Tutorial;
+import com.sprouthub.sprouthub.service.TutorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tutorials")
+public class TutorialController {
+
+    @Autowired
+    private TutorialService tutorialService;
+
+    @PostMapping
+    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+        return ResponseEntity.ok(tutorialService.createTutorial(tutorial));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable String id) {
+        return ResponseEntity.ok(tutorialService.getTutorialById(id));
+    }
+
+
+}
+
+}
+
