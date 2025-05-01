61157729b9ff43215cf5dc89d906f4068745a820
manavi118 <kavindu2002tcc@gmail.com>
2025-05-01 18:52:56 +0530

    v

---
+++ backend/src/main/java/com/sprouthub/sprouthub/controller/MarketplaceController.java
@@ -0,0 +1,43 @@
+package com.sprouthub.sprouthub.controller;
+
+import com.sprouthub.sprouthub.model.Product;
+import com.sprouthub.sprouthub.service.MarketplaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/marketplace")
+public class MarketplaceController {
+
+    @Autowired
+    private MarketplaceService marketplaceService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(marketplaceService.createProduct(product));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(marketplaceService.getProductById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(marketplaceService.getAllProducts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(marketplaceService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        marketplaceService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+}
